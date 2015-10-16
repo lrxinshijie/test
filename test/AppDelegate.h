fba5a5ca6fc7db60ba11d610d7386d2679ce6025
123 <a123@chenxiaoningdeMacBook-Pro.local>
2015-10-16 09:13:37 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 123 on 15/10/16.
+//  Copyright (c) 2015年 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
